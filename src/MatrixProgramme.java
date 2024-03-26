afef72322fd080cb486b79a1c5b08048e51a10fd
cristic1988 <cristic8@gmail.com>
2024-03-26 23:30:36 +0200

    third commit

--- src/MatrixProgramme.java
+++ src/MatrixProgramme.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class MatrixProgramme {
     public static void main(String[] args) {
@@ -28,42 +28,4 @@ public class MatrixProgramme {
     }
 }
 
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Який розмір бажаєте отримати? ->");
-//        int n = sc.nextInt();
-//        int[] a = new int[n];
-//        if (n % 2 == 0) {
-//            for (int i = 0; i < n / 2; i++) {
-//                a[i] = a[n - i - 1] = i + 1;
-//            }
-//        } else {
-//            for (int i = 0; i <= n / 2; i++) {
-//                a[i] = a[n - i - 1] = i + 1;
-//            }
-//        }
-//        System.out.println(Arrays.toString(a));
-//    }
-//    public static void main(String[] args) {
-//        Random random = new Random();
 
-
-//        int[] matrix = new int[] {random.nextInt(8)};
-//        System.out.println(matrix.toString());
-//        System.out.println("Змінена матриця");
-//        int[] temp =
-//        for (int i = 0; i < matrix.length; i++) {
-//            temp = matrix;
-//            if (i % 2 == 0) {
-//                for (int j = temp.length - 1; j >= 0; j--) {
-//                    System.out.printf("%7d", temp[j]);
-//                }
-//            } else {
-//                for (int j = 0; j < temp.length; j++) {
-//                    System.out.printf("%7d", temp[j]);
-//                }
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
